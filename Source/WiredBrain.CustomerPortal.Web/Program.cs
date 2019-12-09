bc9725f103a4592559545aeb59a814a8c21441fb
Geert Huls <geert.huls82@gmail.com>
2019-12-09 22:09:50 +0100

    Add docker setup

--- Source/WiredBrain.CustomerPortal.Web/Program.cs
+++ Source/WiredBrain.CustomerPortal.Web/Program.cs
@@ -20,7 +20,8 @@ namespace WiredBrain.CustomerPortal.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
